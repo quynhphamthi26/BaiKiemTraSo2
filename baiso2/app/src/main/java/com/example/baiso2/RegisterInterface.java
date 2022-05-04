05b58e3611c48fa3e618801a08f1d282cd95eded
Pham Thi Quynh <94799219+quynhphamthi26@users.noreply.github.com>
2022-05-04 08:41:16 +0700

    ptquynh
    ptquynh

---
+++ baiso2/app/src/main/java/com/example/baiso2/RegisterInterface.java
@@ -0,0 +1,21 @@
+package com.example.baiso2;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface RegisterInterface {
+
+    String REGIURL = "http://192.168.1.4/bai2/";
+    @FormUrlEncoded
+    @POST("simpleregister.php")
+    Call<String> getUserRegi(
+            @Field("name") String name,
+            @Field("hobby") String hobby,
+            @Field("username") String uname,
+            @Field("password") String password
+    );
+
+}
+
